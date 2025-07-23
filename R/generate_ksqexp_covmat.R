9390880f37f6f918cb5161f0e9407ab262ef1cd6
LizaHadley <hadleytutoring@gmail.com>
2025-07-23 12:10:52 +0100

    Fixing misunderstanding re axes
    x = ages and y = coverage in this toy model

--- R/generate_ksqexp_covmat.R
+++ R/generate_ksqexp_covmat.R
@@ -1,14 +1,14 @@
 #' Generate a squared exponential covariance matrix
 #' 
-#' @param y1 random vector 1, with elements/ model parameters y1_i (numeric vector)
-#' @param y2 random vector 2, with elements/ model parameters y2_i (numeric vector)
+#' @param x1 random vector 1 of model inputs x1_i (numeric vector)
+#' @param x2 random vector 2 of model inputs x2_i (numeric vector)
 #' @param fn covariance function of two parameters: 
-#'    - abs(y1-y2) which is calculated piecewise in generate_ksqexp_covmat, 
+#'    - abs(x1-x2) which is calculated piecewise in generate_ksqexp_covmat, 
 #'    - lengthscale l. 
 #'    fn is set to ksqexp by default; also works for kexp.
 #' @param l lengthscale
 
-generate_ksqexp_covmat <- function(y1,y2,fn=ksqexp,l) {
-  expand_grid(y1=y1,y2=y2) %>% mutate(k=fn(abs(y1-y2),l)) %>%  # pairwise evaluation of cov function
-    pull(k) %>% matrix(nrow=length(y1),byrow=T)
+generate_ksqexp_covmat <- function(x1,x2,fn=ksqexp,l) {
+  expand_grid(x1=x1,x2=x2) %>% mutate(k=fn(abs(x1-x2),l)) %>%  # pairwise evaluation of cov function
+    pull(k) %>% matrix(nrow=length(x1),byrow=T)
 }
--- R/kexp.R
+++ R/kexp.R
@@ -1,6 +1,6 @@
 #' Calculate the exponential covariance function
 #' 
-#' @param r absolute distance between two points (abs(y1-y2))
+#' @param r absolute distance between two x vectors (abs(x1-x2))
 #' @param l lengthscale
 ksqexp <- function(r,l) {
   exp(-r / l)
