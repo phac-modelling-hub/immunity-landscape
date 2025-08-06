d47aa6083b0ec75673d017dbddffced8cf30b08d
LizaHadley <hadleytutoring@gmail.com>
2025-08-06 12:49:02 +0100

    small updates

--- R/plot_posterior2D.R
+++ R/plot_posterior2D.R
@@ -1,8 +1,7 @@
 #' Plot 2D Gaussian Process posterior
 #' 
 #' This plotting code outputs up to three posterior plots: posterior on [-inf,inf], and the inverse-logit transformed
-#' posterior on [0,1] with and without additional unobserved data points added for comparison.  **Functionality 
-#' not yet added for third plot.
+#' posterior on [0,1] with and without additional unobserved data points added for comparison.
 #' @param df a data frame of draws from the posterior with six required columns: 
 #'  - draw: draw number (integer)
 #'  - x_i: x-values (numeric)
@@ -16,9 +15,16 @@
 #' 
 #' We can also optionally label the plot with three GP model parameters:
 #' @param k_name covariance function name as a character string (character)
-#' @param kparam1 value of first k parameter e.g. lengthscale (numeric)
-#' @param kparam2 value of second k parameter (numeric)
-plot_posterior2D <- function(df, xobs, yobs, zobs, vax_clean=NA, last_agecurrent=NA, k_name=NA, k_param1=NA, k_param2=NA) {
+#' @param k_param1 value of first k parameter e.g. lengthscale (numeric)
+#' @param k_param2 value of second k parameter e.g. relative lengthscale of x to y (numeric)
+#' @param k_param2 value of third k parameter e.g. b (numeric)
+#' 
+#' The final plot also makes use of the full (observed and unobserved) data:
+#' @param vax_dataset data set of observed vaccine coverage data in our standardised format (tibble)
+#' @param last_agecurrent last age for the GP model, i.e. largest x-variable entry (numeric)
+#' @param prov_levels factor defining the names of provinces
+plot_posterior2D <- function(df, xobs, yobs, zobs, k_name=NA, k_param1=NA, k_param2=NA, k_param3=NA,
+                             vax_dataset=NA, last_agecurrent=NA, prov_levels=NA) {
   yobs_factors <- factor(names(yobs), levels(df$y_label))  # convert yobs to factors for use in facet_wrap
   logit_zobs_centred <- logit(zobs) - mean(logit(zobs))  # recall the logistic-transformed data centred around mean 0 for use in plot 1
   
@@ -38,16 +44,17 @@ plot_posterior2D <- function(df, xobs, yobs, zobs, vax_clean=NA, last_agecurrent
     ggtitle(paste0("k=", k_name, "; k_param1=", k_param1, ", k_param2=", k_param2)) +
     facet_wrap(~ y_label) 
   
-  #' #' plot 3
-  #' p3 <- df %>% ggplot() + geom_point(aes(x=x_i, y=post2D_constrained, group=factor(draw)), alpha=0.1) +
-  #'   geom_line(aes(x=x_i, y=post2D_constrained, group=factor(draw)), alpha=0.2) +
-  #'   geom_point(data=tibble(x=(vax_clean %>% filter(age_current<=last_agecurrent) %>% pull(age_current)),  # adding in full data from vax_clean
-  #'                          y=(vax_clean %>% filter(age_current<=last_agecurrent) %>% pull(value)),
-  #'                          y_label=factor((vax_clean %>% filter(age_current<=last_agecurrent) %>% pull(location)), levels=prov_levels)),
-  #'              aes(x=x,y=y), col="green") +
-  #'   geom_point(data=tibble(x=xobs, y=zobs, y_label=yobs_factors), aes(x=x, y=y), col="red") +
-  #'   scale_x_continuous(breaks=unique(df$x_i), name="current age x_i") + scale_y_continuous(limits=c(0,1)) +
-  #'   facet_wrap(~ y_label)
+  #' plot 3
+  p3 <- df %>% ggplot() + geom_point(aes(x=x_i, y=post2D_constrained, group=factor(draw)), alpha=0.1) +
+    geom_line(aes(x=x_i, y=post2D_constrained, group=factor(draw)), alpha=0.2) +
+    geom_point(data=tibble(x=(vax_dataset %>% filter(age_current<=last_agecurrent) %>% pull(age_current)),  # adding in full data from vax_clean
+                           y=(vax_dataset %>% filter(age_current<=last_agecurrent) %>% pull(value)),
+                           y_label=factor((vax_dataset %>% filter(age_current<=last_agecurrent) %>% pull(location)), levels=prov_levels)),
+               aes(x=x,y=y), col="green") +
+    geom_point(data=tibble(x=xobs, y=zobs, y_label=yobs_factors), aes(x=x, y=y), col="red") +
+    scale_x_continuous(breaks=unique(df$x_i), name="current age x_i") + scale_y_continuous(limits=c(0,1)) +
+    ggtitle(paste0("k=", k_name, "; k_param1=", k_param1, ", k_param2=", k_param2, ", k_param=", k_param3)) +
+    facet_wrap(~ y_label)
   
-  list(p1,p2)
+  list(p1,p2,p3)
 }
