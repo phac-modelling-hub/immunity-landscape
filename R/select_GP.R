14fd264d9e04573f929138f36b0b5bcf8fc1d6b3
LizaHadley <hadleytutoring@gmail.com>
2025-10-10 11:02:36 +0100

    minor

--- R/select_GP.R
+++ R/select_GP.R
@@ -1,15 +1,15 @@
 #' GP model selection -- identify optimal combination of k, l1, l2, b. Outputs a tibble detailing 
 #' each set of model hyperparameters and a goodness-of-fit metric (sum(lppd)) using take-one-out cross validation. 
-#' Large lppd is optimal. The recommended parameter values are also printed to the console.
-#' Note: relation of provinces currently uses GDP data and covariance functions can be ksqexp and/or kexp.
+#' Large absolute lppd is optimal. The recommended parameter values are also printed to the console.
+#' Note: covariance functions can be ksqexp and/or kexp.
 #' 
 #' @param vax_dataset data set of observed vaccine coverage data in our standardised format (tibble)
 #' @param last_agecurrent last age for the GP model, i.e. largest x-variable entry (numeric)
-#' @param ndrws specify number of draws to be taken from the prior and posterior distributions (numeric)
+#' @param ndrws specify number of draws to be taken for estimate_lppd() (numeric)
 #' @param prov_values named ordered vector giving numerical y-axis values associated to each province, unscaled
 #' OR a character from the following list:
 #'          - "GDP"
-#'          - ""
+#'          - "low_income_families"
 #' @param k_list a named list of covariance functions, e.g. list(ksqexp = ksqexp, kexp = kexp) (named list)
 #' @param l1 a vector of x-lengthscale parameters for use with covariance functions ksqexp and/or kexp (vector)
 #' @param l2 a vector of relative lengthscale parameters for use with covariance functions ksqexp and/or kexp (vector)
@@ -18,17 +18,17 @@ select_GP <- function(vax_dataset, last_agecurrent=30, prov_values, ndrws=100, k
   # first separate k_list into functions and names
   k_tibble <- tibble(k_name = names(k_list), k = unname(k_list))
   
-  # compute lppd_sum for all combinations of parameters
+  # compute lppd for all combinations of parameters
   combinations <- tidyr::expand_grid(k_tibble, l1, l2, b)  # functions need to be protected in a list
-  combinations <- combinations %>% mutate(lppd_estimate = pmap_dbl(list(k, l1, l2, b), ~ estimate_lppd(  # method 1: estimate (l)ppd
-    vax_dataset = vax_dataset,
-    last_agecurrent = last_agecurrent,
-    prov_values = prov_values,
-    ndrws = ndrws,
-    k = ..1,
-    l1 = ..2,
-    l2 = ..3,
-    b = ..4)))
+  # combinations <- combinations %>% mutate(lppd_estimate = pmap_dbl(list(k, l1, l2, b), ~ estimate_lppd(  # method 1: estimate (l)ppd
+  #   vax_dataset = vax_dataset,
+  #   last_agecurrent = last_agecurrent,
+  #   prov_values = prov_values,
+  #   ndrws = ndrws,
+  #   k = ..1,
+  #   l1 = ..2,
+  #   l2 = ..3,
+  #   b = ..4)))
   combinations <- combinations %>% mutate(lppd_exact = pmap_dbl(list(k, l1, l2, b), ~ compute_lppd(  # method 2: compute lppd analytically
     vax_dataset = vax_dataset,
     last_agecurrent = last_agecurrent,
