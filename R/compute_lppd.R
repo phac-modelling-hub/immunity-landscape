6babc3c75055fa7b9ea72a38bee54c78db477129
Irena Papst <irena.papst@phac-aspc.gc.ca>
2026-04-22 10:26:50 -0400

    refactoring compute_lppd() (with tests)

--- R/compute_lppd.R
+++ R/compute_lppd.R
@@ -56,3 +56,86 @@ compute_lppd <- function(vax_dataset, last_agecurrent=21, prov_values, k=ksqexp,
   # sum lppd values from each test
   return(sum(lppds))
 }
+
+# Refactored to modularize (for accuracy calculation based on PPD means and variances)
+# with help from Claude
+
+#' Prepare inputs needed to compute posterior predictive density (PPD)
+#' @inheritParams compute_lppd2
+#' @return List containing `vax_dataset`, `bottom` (matrix for denominator of PPD mean equation), logit_zobs_centred (centred and transformed response values), and centring_term (mean of transformed response values used to centre them)
+prepare_ppd_inputs <- function(vax_dataset, last_agecurrent = 21, prov_values, k = ksqexp, l1 = NA, l2 = NA, b = 1, meas_error = NA) {
+vax_dataset <- vax_dataset %>% filter((age_current <= last_agecurrent) & n_doses != "2+")
+xobs <- vax_dataset %>% pull(age_current)
+prov_values <- extract_province_relation(prov_values, vax_dataset = vax_dataset)
+prov_values <- prov_values * l2
+yobs <- prov_values[vax_dataset %>% pull(location)]
+zobs <- vax_dataset %>% pull(value)
+centring_term <- mean(LaplacesDemon::logit(zobs))
+logit_zobs_centred <- LaplacesDemon::logit(zobs) - centring_term
+
+xyobs <- tibble(x = xobs, y = yobs)
+koo <- generate_2Dksqexp_covmat(xyobs, xyobs, fn = k, l = l1, b = b) + 1e-12 * diag(nrow(xyobs))
+
+bottom <- if (is.na(meas_error)) {
+solve(koo)
+} else {
+solve(koo + diag(meas_error^2, nrow(koo)))
+}
+
+list(
+vax_dataset = vax_dataset,
+bottom = bottom,
+logit_zobs_centred = logit_zobs_centred,
+centring_term = centring_term
+)
+}
+
+#' Compute parameters for posterior predictive density (PPD)
+#' @param prep Output from `prepare_ppd_inputs`
+#' @return List containing `ppd_means` and `ppd_vars`
+compute_ppd_params <- function(prep) {
+bottom <- prep$bottom
+top <- bottom %*% prep$logit_zobs_centred
+n <- nrow(prep$vax_dataset)
+zobs <- prep$vax_dataset %>% pull(value)
+
+ppd_means <- rep(NA, n)
+ppd_vars <- rep(NA, n)
+for (i in seq_len(n)) {
+logit_zout_centred <- LaplacesDemon::logit(zobs[i]) - prep$centring_term
+ppd_means[i] <- logit_zout_centred - (top[i] / bottom[i, i])
+ppd_vars[i] <- 1 / bottom[i, i]
+}
+
+list(ppd_means = ppd_means, ppd_vars = ppd_vars)
+}
+
+#' COMPUTE the lppd analytically for one GP model (i.e. for one choice of parameter values of {k, l1, l2, b, meas_error}) from
+#' leave-one-out cross-validation. 
+#' See GitHub Issue #4 for details of method.
+#' Note: scaling by l2 happens inside the function.
+#' 
+#' @param vax_dataset data set of observed vaccine coverage data in our standardised format (tibble)
+#' @param last_agecurrent last age for the GP model, i.e. largest x-variable entry (numeric)
+#' @param prov_values named ordered vector giving numerical y-axis values associated to each province, unscaled
+#' OR a character from the following list:
+#'          - "GDP"
+#'          - "low_income_families"
+#'          - "vaccine_hesitancy".
+#' @param k covariance function chosen from the following list: (function) **Functionality not added yet for matern.
+#'          - ksqexp,
+#'          - kexp,
+#'          - kmatern.
+#' @param l1 x-lengthscale parameter, for use with covariance functions ksqexp and/or kexp
+#' @param l2 relative lengthscale of x values to y values, for use with covariance functions ksqexp and kexp
+#' @param b scale for covariance function determining the output variance
+#' @param meas_error if specified, measurement error is included (numeric)
+compute_lppd2 <- function(vax_dataset, last_agecurrent = 21, prov_values, k = ksqexp, l1 = NA, l2 = NA, b = 1, meas_error = NA) {
+ppd_inputs <- prepare_ppd_inputs(vax_dataset, last_agecurrent, prov_values, k, l1, l2, b, meas_error)
+ppd_params <- compute_ppd_params(ppd_inputs)
+
+lppds <- -0.5 * log(ppd_params$ppd_vars) -
+((ppd_inputs$logit_zobs_centred - ppd_params$ppd_means)^2 / (2 * ppd_params$ppd_vars)) -
+0.5 * log(2 * pi)
+sum(lppds)
+}
