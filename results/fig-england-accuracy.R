d92e938e5cbc8a7397065c0682addd34a677e059
Irena Papst <irena.papst@phac-aspc.gc.ca>
2026-06-03 15:57:13 -0400

    integrate code for england accuracy experiment into ms figs workflow

--- results/fig-england-accuracy.R
+++ results/fig-england-accuracy.R
@@ -1,6 +1,3 @@
-library(dplyr)
-library(ggplot2)
-
 # Load data from pipeline
 targets::tar_load(accuracy)
 targets::tar_load(error_tolerance)
@@ -17,7 +14,7 @@ df <- purrr::map(branch_names, function(nm) {
 # --- Across reps
 
 # Distribution of accuracy across reps
-ggplot(df, aes(x = score)) +
+fig_england_accuracy <- ggplot(df, aes(x = score)) +
   geom_histogram(bins = 20) +
   geom_vline(xintercept = mean(df$score), linetype = "dashed", color = "red") +
   annotate(
@@ -36,32 +33,4 @@ ggplot(df, aes(x = score)) +
     subtitle = glue::glue("across all {length(unique(df$id_rep))} subsamplings of England data (error tolerance: {scales::percent_format()(error_tolerance)})"),
     x = "Accuracy per test point",
     y = "Count of test points"
-  )
-
-# --- By rep
-
-# Distribution of accuracy by rep
-ggplot(df, aes(x = score)) +
-  geom_histogram(bins = n_bins) +
-  geom_vline(xintercept = mean(df$score), linetype = "dashed", color = "red") +
-  scale_x_continuous(labels = scales::percent_format()) +
-  facet_wrap(~id_rep) +
-  labs(
-    title = "Distribution of accuracy scores by replication",
-    subtitle = glue::glue("error tolerance: {scales::percent_format()(error_tolerance)}"),
-    x = "Accuracy per test point",
-    y = "Count of test points"
-  )
-
-# Average accuracy by rep
-df |>
-  group_by(id_rep) |>
-  summarize(mean_score = mean(score), .groups = "drop") |>
-  ggplot(aes(x = mean_score)) +
-  geom_histogram(bins = 10) +
-  labs(
-    title = "Mean accuracy per replication",
-    subtitle = glue::glue("error tolerance: {scales::percent_format()(error_tolerance)}"),
-    x = "Mean accuracy per replication",
-    y = "Count"
   )
